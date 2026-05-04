d986dd69351ebeee78e56946225e2f1f9dd464d0
Piyush Boudh <piyushboudh94@gmail.com>
2026-05-04 12:19:45 +0530

    fix: accept Railway Postgres env vars

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -4,13 +4,18 @@ import * as schema from "./schema";
 
 const { Pool } = pg;
 
-if (!process.env.DATABASE_URL) {
+const connectionString =
+  process.env.DATABASE_URL ??
+  process.env.POSTGRES_URL ??
+  process.env.POSTGRES_URL_NON_POOLING;
+
+if (!connectionString) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
+    "DATABASE_URL (or POSTGRES_URL/POSTGRES_URL_NON_POOLING) must be set. Did you forget to provision a database?",
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ connectionString });
 export const db = drizzle(pool, { schema });
 
 export * from "./schema";
